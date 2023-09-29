2468f7bba596daf1fa6fcd44fb7cfe4f02f783d5
datroiano <deantroiano123@gmail.com>
2023-09-29 14:01:39 -0400

    init

--- OptionsOperations/options_contract_object.py
+++ OptionsOperations/options_contract_object.py
@@ -19,13 +19,27 @@ class OptionsContract:
         self.expiration_date = str(expiration_date)
         self.contract_type = contract_type
 
-    def pull_options_price_data(self, from_date, to_date, window_start_time, window_end_time, timespan,
-                                polygon_api_key='r1Jqp6JzYYhbt9ak10x9zOpoj1bf58Zz', multiplier=1):
+
+class OptionsContractsPriceData(OptionsContract):
+    def __init__(self, options_contract=None, from_date=None, to_date=None, window_start_time=None, window_end_time=None,
+                 timespan=None, polygon_api_key='r1Jqp6JzYYhbt9ak10x9zOpoj1bf58Zz', multiplier=1):
+        super().__init__(options_contract.ticker, options_contract.strike, options_contract.expiration_date,
+                             options_contract.contract_type)
+
+        self.polygon_api_key = polygon_api_key
+        self.from_date = from_date
+        self.to_date = to_date
+        self.window_start_time = window_start_time
+        self.window_end_time = window_end_time
+        self.timespan = timespan
+        self.multiplier = multiplier
+
+    def pull_options_price_data(self):
         exp_day = int(self.expiration_date[8:])  # 2023-09-29
         exp_month = int(self.expiration_date[5:7])
         exp_year = int(self.expiration_date[2:4])
-        from_date = to_unix_time(f'{from_date} {window_start_time}')
-        to_date = to_unix_time(f'{to_date} {window_end_time}')
+        from_date = to_unix_time(f'{self.from_date} {self.window_start_time}')
+        to_date = to_unix_time(f'{self.to_date} {self.window_end_time}')
 
         options_ticker = create_options_ticker(ticker=self.ticker, strike=self.strike, expiration_year=exp_year,
                                                expiration_month=exp_month, expiration_day=exp_day,
@@ -33,11 +47,11 @@ class OptionsContract:
 
         # Polygon verification
         headers = {
-            "Authorization": f"Bearer {polygon_api_key}"
+            "Authorization": f"Bearer {self.polygon_api_key}"
         }
 
         # Polygon data endpoint
-        endpoint = f"https://api.polygon.io/v2/aggs/ticker/{options_ticker}/range/{multiplier}/{timespan}/{from_date}/{to_date}"
+        endpoint = f"https://api.polygon.io/v2/aggs/ticker/{options_ticker}/range/{self.multiplier}/{self.timespan}/{from_date}/{to_date}"
 
         # Getting the options data as requested
         response = requests.get(endpoint, headers=headers).json()
@@ -45,33 +59,78 @@ class OptionsContract:
         if response['queryCount'] == 0 or response['status'] == 'ERROR':  # Return None value
             return None
 
-        # Make a readable python dictionary based on this
-        price_data = response['results']
-
-        cleaned_response = dict()
-        for timestamp in price_data:
-            quote_time = timestamp['t']
-            cleaned_response[quote_time] = {
-                'volume': timestamp['v'],
-                'volume_weighted': timestamp['vw'],
-                'open': timestamp['o'],
-                'close': timestamp['c'],
-                'high': timestamp['h'],
-                'low': timestamp['l'],
-                'number': timestamp['n']
-            }
+        cleaned_response = {
+            from_unix_time(timestamp['t']): {'volume': timestamp['v'], 'volume_weighted': timestamp['vw'], 'open': timestamp['o'],
+                             'close': timestamp['c'], 'high': timestamp['h'], 'low': timestamp['l'],
+                             'number': timestamp['n']} for timestamp in response['results']}
 
         return cleaned_response
 
+    def get_query_count_for_timeperiod(self):
+        exp_day = int(self.expiration_date[8:])  # 2023-09-29
+        exp_month = int(self.expiration_date[5:7])
+        exp_year = int(self.expiration_date[2:4])
+        from_date = to_unix_time(f'{self.from_date} {self.window_start_time}')
+        to_date = to_unix_time(f'{self.to_date} {self.window_end_time}')
+
+        options_ticker = create_options_ticker(ticker=self.ticker, strike=self.strike, expiration_year=exp_year,
+                                               expiration_month=exp_month, expiration_day=exp_day,
+                                               contract_type=self.contract_type)
+
+        # Polygon verification
+        headers = {
+            "Authorization": f"Bearer {self.polygon_api_key}"
+        }
+
+        # Polygon data endpoint
+        endpoint = f"https://api.polygon.io/v2/aggs/ticker/{options_ticker}/range/{self.multiplier}/{self.timespan}/{from_date}/{to_date}"
+
+        # Getting the options data as requested
+        response = requests.get(endpoint, headers=headers).json()
+
+        if response['queryCount'] == 0 or response['status'] == 'ERROR':  # Return None value
+            return None
+        else:
+            return response['queryCount']
+
+    def get_raw_data(self):
+        exp_day = int(self.expiration_date[8:])  # 2023-09-29
+        exp_month = int(self.expiration_date[5:7])
+        exp_year = int(self.expiration_date[2:4])
+        from_date = to_unix_time(f'{self.from_date} {self.window_start_time}')
+        to_date = to_unix_time(f'{self.to_date} {self.window_end_time}')
+
+        options_ticker = create_options_ticker(ticker=self.ticker, strike=self.strike, expiration_year=exp_year,
+                                               expiration_month=exp_month, expiration_day=exp_day,
+                                               contract_type=self.contract_type)
+
+        # Polygon verification
+        headers = {
+            "Authorization": f"Bearer {self.polygon_api_key}"
+        }
+
+        # Polygon data endpoint
+        endpoint = f"https://api.polygon.io/v2/aggs/ticker/{options_ticker}/range/{self.multiplier}/{self.timespan}/{from_date}/{to_date}"
+
+        # Getting the options data as requested
+        response = requests.get(endpoint, headers=headers).json()
+
+        if response['queryCount'] == 0 or response['status'] == 'ERROR':  # Return None value
+            return None
+        else:
+            return response
 
 
 
 
-# Test instance (Below)
-# test_contract = OptionsContract("AAPL", 180, '2023-09-29')
-# result = test_contract.pull_options_price_data(from_date='2023-09-22', to_date='2023-09-22',
-#                                                window_start_time='09:30:00', window_end_time='16:30:00',
-#                                                timespan='minute')
 
+# Using an OptionsContract instance
+test_contract = OptionsContract("AAPL", 180, '2023-09-29')
+test_contract_data = OptionsContractsPriceData(options_contract=test_contract,
+                                               from_date='2023-09-28', to_date='2023-09-28',
+                                               window_start_time='09:30:00', window_end_time='16:30:00',
+                                               timespan='minute')
+
+print(test_contract_data.pull_options_price_data())
 
 
