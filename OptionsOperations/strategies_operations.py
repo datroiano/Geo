8373f27b413fa5fa6731d174a1e31443bb18921e
datroiano <deantroiano123@gmail.com>
2023-10-02 17:05:41 -0400

    init

--- OptionsOperations/strategies_operations.py
+++ OptionsOperations/strategies_operations.py
@@ -89,20 +89,51 @@ class SingleOptionStrategy:
             "profit_loss_percent": profit_loss_percent,
             "size": size,
             "pricing": pricing
-            }
+        }
         }
 
         return trade_array
 
 
+class TwoOptionStrategy:
+    def __init__(self, cleaned_data_op_1, cleaned_data_op_2):
+        self.op_1_data = cleaned_data_op_1
+        self.op_2_data = cleaned_data_op_2
+
+    def long_strangle_simulation(self, entry_window_start, entry_window_end, exit_window_start, exit_window_end,
+                                 size=1, pricing=1):
+        entry_window_start = int(to_unix_time(entry_window_start))
+        entry_window_end = int(to_unix_time(entry_window_end))
+        exit_window_start = int(to_unix_time(exit_window_start))
+        exit_window_end = int(to_unix_time(exit_window_end))
+
+        # Create a set of unix_time values from op_2_data for faster lookup
+        op_2_unix_times = set(value['unix_time'] for value in self.op_2_data.values())
+
+        # Create a new dictionary containing only the elements from op_1_data with unix_time in op_2_unix_times
+        self.op_1_data = {key: value for key, value in self.op_1_data.items() if value['unix_time'] in op_2_unix_times}
+
+        entry_data = [value for value in self.op_1_data.values() if
+                      entry_window_start <= value['unix_time'] <= entry_window_end]
+
+        exit_data = [value for value in self.op_1_data.values() if
+                     exit_window_start <= value['unix_time'] <= exit_window_end]
+
+        # Do nested for loop to run simulation for each entry/end time pair here
 
 
-test_contract = oc.OptionsContract("AAPL", 170, '2023-09-29')
-test_contract_data = oc.OptionsContractsPriceData(options_contract=test_contract,
-                                                  from_date='2023-09-28', to_date='2023-09-28',
-                                                  window_start_time='09:30:00', window_end_time='16:30:00',
-                                                  timespan='minute')
+contract1 = oc.OptionsContract("AAPL", 170, '2023-09-29', contract_type=True)
+contract1data = oc.OptionsContractsPriceData(options_contract=contract1,
+                                             from_date='2023-09-28', to_date='2023-09-28',
+                                             window_start_time='09:30:00', window_end_time='16:30:00',
+                                             timespan='minute')
 
-strategy = SingleOptionStrategy(test_contract_data.pull_options_price_data())
-trade_example = strategy.calculate_profit_timed_entry('2023-09-28 09:30:00', '2023-09-28 13:00:00')
+contract2 = oc.OptionsContract("AAPL", 170, '2023-09-29', contract_type=False)
+contract2data = oc.OptionsContractsPriceData(options_contract=contract1,
+                                             from_date='2023-09-28', to_date='2023-09-28',
+                                             window_start_time='09:30:00', window_end_time='16:30:00',
+                                             timespan='minute')
 
+simulation = TwoOptionStrategy(contract1data.pull_options_price_data(), contract2data.pull_options_price_data())
+simulation.long_strangle_simulation(entry_window_start='2023-09-28 09:30:00', entry_window_end='2023-09-28 11:30:00',
+                                    exit_window_start='2023-09-28 11:30:00', exit_window_end='2023-09-28 14:30:00')
