6e50421858a0227088fbbfcf73aa46bacf2cae1d
datroiano <deantroiano123@gmail.com>
2023-11-30 13:14:30 -0500

    init

--- OptionsOperations/earnings_calendar_pulls.py
+++ OptionsOperations/earnings_calendar_pulls.py
@@ -15,6 +15,9 @@ class TestCompanies:
         # Make finnhub client request to retrieve tickers, report dates, and time period
         self.symbols_list = self.finnhub_retrieval()
 
+        # Make initial Polygon request to retrieve average price
+        self.price_averages = self.polygon_retrieval(avg_time_start="09:30:00", avg_time_end="11:00:00")
+
     def finnhub_retrieval(self):
         finnhub_client = finnhub.Client(api_key="ck45p3hr01qus81pq4u0ck45p3hr01qus81pq4ug")
 
@@ -40,52 +43,65 @@ class TestCompanies:
         return symbols_list
 
     def polygon_retrieval(self, avg_time_start, avg_time_end, polygon_api_key='r1Jqp6JzYYhbt9ak10x9zOpoj1bf58Zz'):
+        price_averages = []
         i = 0
+
+        headers = {
+            "Authorization": f"Bearer {polygon_api_key}"
+        }
+        multiplier = "1"
+        timespan = "minute"
+
         for item in self.symbols_list:
             if item["date"] is None or item["symbol"] is None:
                 continue
             else:
-                while i < 1:
-                    headers = {
-                        "Authorization": f"Bearer {polygon_api_key}"
-                    }
-                    multiplier = "1"
-                    timespan = "minute"
-                    ticker = item['symbol']
-                    from_date = item['date']
-                    to_date = item['date']
+                ticker = item['symbol']
+                from_date = item['date']
+                to_date = item['date']
 
-                    endpoint = f"https://api.polygon.io/v2/aggs/ticker/{ticker}/range/{multiplier}/{timespan}/{self.from_date}/{self.to_date}"
+                endpoint = f"https://api.polygon.io/v2/aggs/ticker/{ticker}/range/{multiplier}/{timespan}/{from_date}/{to_date}"
 
-                    response = requests.get(endpoint, headers=headers).json()
+                response = requests.get(endpoint, headers=headers).json()
 
-                    processed_data = []
-                    for result in response['results']:
-                        if int(to_unix_time(f'{from_date} {avg_time_start}')) <= int(result['t']) <= int(to_unix_time(f'{to_date} {avg_time_end}')):
-                            ticker = response['ticker']
-                            time = from_unix_time(result['t'])
-                            high = result['h']
-                            low = result['l']
+                processed_data = []
+                for result in response['results']:
+                    if int(to_unix_time(f'{from_date} {avg_time_start}')) <= int(result['t']) <= int(to_unix_time(f'{to_date} {avg_time_end}')):
+                        ticker = response['ticker']
+                        time = from_unix_time(result['t'])
+                        high = result['h']
+                        low = result['l']
 
-                            processed_data.append({
-                                'ticker': ticker,
-                                'date': time,
-                                'high': high,
-                                'low': low
-                            })
+                        processed_data.append({
+                            'ticker': ticker,
+                            'date': time,
+                            'high': high,
+                            'low': low
+                        })
 
-                    # raw_prices = []
-                    # for item in processed_data:
-                    #     raw_prices.append((item['high'] + item['low']) / 2)
+                raw_prices = []
+                for data_point in processed_data:
+                    raw_prices.append((data_point['high'] + data_point['low']) / 2)
 
-                    return to_unix_time(f'{from_date} {avg_time_start}')
+                new_entry = {
+                    'symbol': ticker,
+                    'avg_price': round(statistics.mean(raw_prices), ndigits=2)
+                }
+
+                price_averages.append(new_entry)
+
+                i += 1
+                if i >= 4:
+                    break
+
+        return price_averages
+
 
 
 
 
 x = TestCompanies(min_revenue=1000000000, from_date="2023-11-01", to_date="2023-11-29", report_hour="amc")
-print(x.polygon_retrieval("09:30:00", "11:00:00"))
-# print(x.symbols_list)
+print(x.price_averages)
 
 
 # url = "https://api.tdameritrade.com/v1/marketdata/chains"
